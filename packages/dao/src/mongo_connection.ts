60035ce54df089b3ca35e85fae9f8854a7481d2a
Eiko Thomas <eiko.thomas@gmail.com>
2022-02-06 22:02:21 +0100

    wip: first mongo connection - ...

--- packages/dao/src/mongo_connection.ts
+++ packages/dao/src/mongo_connection.ts
@@ -1,20 +1,45 @@
 import * as mongoDB from "mongodb";
-import * as dotenv from "dotenv";
+
+export interface MongoConnectionConfig {
+    conStr: string,
+    dbName: string
+}
+
+
+let defaultClient: mongoDB.MongoClient;
+let defaultConStr: string;
+let defaultDbName: string;
+let defaultDb: mongoDB.Db;
 
 /*
-export async function getConnection () {
-    dotenv.config();
- 
-    const client: mongoDB.MongoClient = new mongoDB.MongoClient(process.env.DB_CONN_STRING);
-            
-    await client.connect();
-        
+function initDefaultDbConnection(): Promise<mongoDB.MongoClient> {
+    defaultConStr = "TODO";
+    defaultClient = new mongoDB.MongoClient(defaultConStr);
+    return defaultClient.connect();
+}
+*/
+
+export async function getDb (databaseConf?: MongoConnectionConfig | string): Promise<mongoDB.Db> {
+/*
+    if (databaseConf) {
+        // return a new DB connection
+        // TODO
+    }
+    else {
+        if (!defaultClient) {
+            initDefaultDbConnection()
+            .then( mongoClient => {
+
+            })
+            .catch(e => {
+                // TODO handle error
+            }
+        }
+    }
+    const client: mongoDB.MongoClient = new mongoDB.MongoClient(defaultConStr);
+    client.connect();
     const db: mongoDB.Db = client.db(process.env.DB_NAME);
-   
-    const gamesCollection: mongoDB.Collection = db.collection(process.env.GAMES_COLLECTION_NAME);
- 
-  collections.games = gamesCollection;
-       
-         console.log(`Successfully connected to database: ${db.databaseName} and collection: ${gamesCollection.collectionName}`);
+*/
+    return null;
  }
- */
+ 
