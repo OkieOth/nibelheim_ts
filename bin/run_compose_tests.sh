235fe64490e243c50a5b67e712b2e6a06de7b4fa
Eiko Thomas <eiko.thomas@gmail.com>
2022-02-14 20:12:12 +0100

    enable docker-compose based tests

---
+++ bin/run_compose_tests.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+scriptPos=${0%/*}
+
+cd $scriptPos/..
+
+if docker-compose -p nibelheim_ts_test -f $scriptPos/../docker-compose/test.yaml up --no-deps --build --exit-code-from nibelheim
+then
+    echo 'everything fine :)'
+else
+    echo 'something went wrong :-('
+fi
