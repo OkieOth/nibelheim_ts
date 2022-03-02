20d7591e5d7d310feaa3b8ce694d06aada6bf530
Eiko Thomas <eiko.thomas@gmail.com>
2022-03-02 23:32:50 +0100

    edit dao find template

--- packages/dao/src_generated/dao_find.ts
+++ packages/dao/src_generated/dao_find.ts
@@ -40,6 +40,37 @@ export async function findMine(dbName: string, collectionName?: string): Promise
     });
 }
 
+export async function findMineByObjectId(objId: string, dbName: string, collectionName?: string): Promise<types.Mine> {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const collectionNameToUse = ! collectionName ? "Mine" : collectionName;
+            const db: mongoDb.Db = await mongoConnection.getDb(dbName);
+            const collection: mongoDb.Collection = db.collection(collectionNameToUse);
+            const filter = new mongoDb.ObjectId(objId);
+            const result = await collection.findOne({_id: filter});
+            if (!result) {
+                logger.info(() => `found no element in db: ${dbName}, collection: ${collectionNameToUse}, _id=${objId}`, "findMineByObjectId");
+                return resolve(null);
+            }
+            // TODO check if type or child contains UuuiType
+            dao_uuid.dao2Mine(result);
+            if (types.isMine(result)) {
+                logger.info(() => `found element in db: ${dbName}, collection: ${collectionNameToUse}, _id=${objId}`, "findMineByObjectId");
+                resolve(result);
+            }
+            else {
+                const errorMsg = `found something in db, but it has wrong type: ${dbName}, collection: ${collectionNameToUse}, _id=${objId}`;
+                logger.info(errorMsg, "findMineByObjectId");
+                reject(errorMsg);
+            }
+        }
+        catch(e) {
+            logger.error(e);
+            reject(e);
+        }
+    });
+}
+
 
 export async function findMineByKey(key: string, dbName: string, collectionName?: string): Promise<types.Mine> {
     return new Promise(async (resolve, reject) => {
@@ -100,6 +131,37 @@ export async function findMineSpotRow(dbName: string, collectionName?: string): 
     });
 }
 
+export async function findMineSpotRowByObjectId(objId: string, dbName: string, collectionName?: string): Promise<types.MineSpotRow> {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const collectionNameToUse = ! collectionName ? "MineSpotRow" : collectionName;
+            const db: mongoDb.Db = await mongoConnection.getDb(dbName);
+            const collection: mongoDb.Collection = db.collection(collectionNameToUse);
+            const filter = new mongoDb.ObjectId(objId);
+            const result = await collection.findOne({_id: filter});
+            if (!result) {
+                logger.info(() => `found no element in db: ${dbName}, collection: ${collectionNameToUse}, _id=${objId}`, "findMineSpotRowByObjectId");
+                return resolve(null);
+            }
+            // TODO check if type or child contains UuuiType
+            dao_uuid.dao2MineSpotRow(result);
+            if (types.isMineSpotRow(result)) {
+                logger.info(() => `found element in db: ${dbName}, collection: ${collectionNameToUse}, _id=${objId}`, "findMineSpotRowByObjectId");
+                resolve(result);
+            }
+            else {
+                const errorMsg = `found something in db, but it has wrong type: ${dbName}, collection: ${collectionNameToUse}, _id=${objId}`;
+                logger.info(errorMsg, "findMineSpotRowByObjectId");
+                reject(errorMsg);
+            }
+        }
+        catch(e) {
+            logger.error(e);
+            reject(e);
+        }
+    });
+}
+
 export async function findDwarf(dbName: string, collectionName?: string): Promise<types.Dwarf[]> {
     return new Promise(async (resolve, reject) => {
         try {
@@ -120,6 +182,37 @@ export async function findDwarf(dbName: string, collectionName?: string): Promis
             });
             cursor.close();
             resolve(array);
+        }
+        catch(e) {
+            logger.error(e);
+            reject(e);
+        }
+    });
+}
+
+export async function findDwarfByObjectId(objId: string, dbName: string, collectionName?: string): Promise<types.Dwarf> {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const collectionNameToUse = ! collectionName ? "Dwarf" : collectionName;
+            const db: mongoDb.Db = await mongoConnection.getDb(dbName);
+            const collection: mongoDb.Collection = db.collection(collectionNameToUse);
+            const filter = new mongoDb.ObjectId(objId);
+            const result = await collection.findOne({_id: filter});
+            if (!result) {
+                logger.info(() => `found no element in db: ${dbName}, collection: ${collectionNameToUse}, _id=${objId}`, "findDwarfByObjectId");
+                return resolve(null);
+            }
+            // TODO check if type or child contains UuuiType
+            dao_uuid.dao2Dwarf(result);
+            if (types.isDwarf(result)) {
+                logger.info(() => `found element in db: ${dbName}, collection: ${collectionNameToUse}, _id=${objId}`, "findDwarfByObjectId");
+                resolve(result);
+            }
+            else {
+                const errorMsg = `found something in db, but it has wrong type: ${dbName}, collection: ${collectionNameToUse}, _id=${objId}`;
+                logger.info(errorMsg, "findDwarfByObjectId");
+                reject(errorMsg);
+            }
         }
         catch(e) {
             logger.error(e);
