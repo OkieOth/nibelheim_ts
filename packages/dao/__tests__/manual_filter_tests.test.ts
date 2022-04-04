b912bac90678df86cab0df6fe33ca22a653fa7ec
Eiko Thomas <eiko.thomas@gmail.com>
2022-04-04 22:15:34 +0200

    add uuid and enum filter

---
+++ packages/dao/__tests__/manual_filter_tests.test.ts
@@ -0,0 +1,75 @@
+import { assert, expect } from 'chai';
+import * as dotenv from "dotenv";
+import {logger} from "logger";
+import  * as fs from "fs";
+import * as dummy from "types_random";
+import * as filter from "filter";
+import * as mongoConnection from "../src/mongo_connection";
+import * as dao_find from "../src_generated/dao_find";
+import * as dao_insert from "../src_generated/dao_insert";
+import {NO_FILTER, NO_LIMIT, NO_SORT} from "../src/mongo_helper";
+import * as types from "types";
+import * as findTypes from "../src_generated/dao_find_types";
+
+const envPath1 = "packages/dao/__tests__/singleMongo";
+if (fs.existsSync(envPath1)) {
+    dotenv.config({ path: `${envPath1}/.env` });
+} else {
+    dotenv.config({ path: "__tests__/singleMongo/.env" });
+}
+
+const errorPromise = (msg) => {
+    return new Promise((resovle, reject) => {
+        reject(msg);
+    });
+}
+
+//const testDb = `nibelheim_test_${new Date().getTime()}`;
+const testDb = `nibelheim_test_manual_filter`;
+
+function* indexGenerator(maxItems) {
+    let index = 0;
+    while (index < maxItems) {
+      yield index;
+      index++;
+    }
+}
+
+const randomInserts = 10;
+
+
+describe('some filter tests', async () => {
+    it('simple equal', function (done) {
+        try {
+            /*
+            let promises = [];
+            for (const num of indexGenerator(randomInserts)) {
+                const x: types.Mine = dummy.randomMine()
+                promises.push(dao_insert.insertMine(x, testDb));
+            }
+            Promise.all(promises).then(function(){
+            */
+                // all inserts are done
+                const f = findTypes.createMineFilterName(filter.StringFilterOperator.EQ, ["1o^tcFEo]P4"])
+                dao_find.findMine([f], NO_SORT, 0, NO_LIMIT,testDb)
+                .then(found => {
+                    mongoConnection.closeDefaultConnection();
+                    if (found.length != randomInserts) {
+                        done(`received wrong number of elements. Expected ${randomInserts} got ${found.length}`);
+                        return;
+                    }
+                    if (!types.isMineArray(found)) {
+                        done("expected Mine array, but got something different");
+                        return;
+                    }
+                    logger.info("done :)");
+                    done();
+                });
+//            });
+        }
+        catch(e) {
+            mongoConnection.closeDefaultConnection();
+            done(`can't connect to db: ${e}`);
+        }
+    });
+});
--- packages/dao/src/mongo_helper.ts
+++ packages/dao/src/mongo_helper.ts
@@ -1,6 +1,7 @@
 import * as mongoDb from "mongodb";
 import * as filterExt from "../src/filter_types_ext"
 import * as filter from "filter";
+import {logger} from "logger";
 
 export const NO_LIMIT=0;
 
@@ -8,8 +9,79 @@ export const NO_FILTER=[];
 
 export const NO_SORT=[];
 
+function processDateFilter(field: string, filter: filter.DateFilter, retObj: object) {
+/*
+    switch (filter.operator) {
+        case :
+            break;
+        default:
+            logger.error("unknown string filter operator","processStringFilter");
+            return;
+    }
+*/
+    // TODO
+}
+function processBooleanFilter(field: string, filter: filter.BooleanFilter, retObj: object) {
+/*
+    switch (filter.operator) {
+        case :
+            break;
+        default:
+            logger.error("unknown string filter operator","processStringFilter");
+            return;
+    }
+*/
+    // TODO
+}
+function processNumFilter(field: string, filter: filter.NumericFilter, retObj: object) {
+/*
+    switch (filter.operator) {
+        case :
+            break;
+        default:
+            logger.error("unknown string filter operator","processStringFilter");
+            return;
+    }
+*/
+    // TODO
+}
+function processStringFilter(field: string, filter: filter.StringFilter, retObj: object) {
+    // TODO
+    switch (filter.operator) {
+        case :
+            break;
+        default:
+            logger.error("unknown string filter operator","processStringFilter");
+            return;
+    }
+    const eqExpression = {
+        "$eq": filter.values[0]
+    };
+    retObj[field] = eqExpression;
+    return retObj;
+}
+
+
 export function getMongoFilter(filter: filterExt.DaoFieldFilter[]): object {
-    return {}; // TODO
+    const retObj = {};
+    filter.forEach(f => {
+        if (f.dateFilter) {
+            processDateFilter(f.field, f.dateFilter, retObj);
+        }
+        else if (f.boolFilter) {
+            processBooleanFilter(f.field, f.boolFilter, retObj);
+        }
+        else if (f.numFilter) {
+            processNumFilter(f.field, f.numFilter, retObj);
+        }
+        else if (f.strFilter) {
+            processStringFilter(f.field, f.strFilter, retObj);
+        }
+        else {
+            logger.error("unknown filter type","getMongoFilter");
+        }
+    });
+    return retObj;
 }
 
 export function getMongoSort(sort: filter.FieldSort[]): mongoDb.Sort {
